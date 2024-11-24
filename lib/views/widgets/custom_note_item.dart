6b405a61945fb488534d2a1c1c0e3df17744f494
blalfayed <fayedblal@yahoo.com>
2024-11-24 22:33:13 +0200

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,47 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'FLutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'build your career with Belal Fayed',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(.4), fontSize: 18),
-              ),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 25,
-                )),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
           ),
-          Padding(
-            padding: EdgeInsets.only(right: 24),
-            child: Text('Nov23,2024',
-                style: TextStyle(color: Colors.black.withOpacity(.5))),
-          )
-        ],
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'FLutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'build your career with Belal Fayed',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.4), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 25,
+                  )),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 24),
+              child: Text('Nov23,2024',
+                  style: TextStyle(color: Colors.black.withOpacity(.5))),
+            )
+          ],
+        ),
       ),
     );
   }
